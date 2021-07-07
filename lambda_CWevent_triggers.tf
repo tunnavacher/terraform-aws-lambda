6d8088b37015b8c39371bb4b34c5ed74b709fd5f
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-07-07 17:31:37 +0530

    Update lambda_CWevent_triggers.tf

--- lambda_CWevent_triggers.tf
+++ lambda_CWevent_triggers.tf
@@ -1,13 +1,19 @@
 # Adding Cloudwatch events as trigger to my lambda and giving the permissions
 ##################
-resource "aws_cloudwatch_event_rule" "lambda_trigger_every_five_minutes" {
-    name = var.cwrulename
-    description = "Fires every five minutes"
-    schedule_expression = var.Schedule
+resource "aws_cloudwatch_event_rule" "lambda_trigger_after_crawler_succeded" {
+  name        = var.cwrulename
+  description = "after crawler run successfully"
+  event_pattern = jsonencode("${path.module}/cloudwatch_rule.json")
 }
 
-resource "aws_cloudwatch_event_target" "lambda_trigger_every_five_minutes" {
-    rule = "${aws_cloudwatch_event_rule.lambda_trigger_every_five_minutes.name}"
+#resource "aws_cloudwatch_event_rule" "lambda_trigger_every_five_minutes" {
+    #name = var.cwrulename
+    #description = "Fires every five minutes"
+    #schedule_expression = var.Schedule
+#}
+
+resource "aws_cloudwatch_event_target" "lambda_trigger_after_crawler_succeded" {
+    rule = "${aws_cloudwatch_event_rule.lambda_trigger_after_crawler_succeded}"
     target_id = var.target_id
     arn = aws_lambda_function.test_lambda.arn
 }
@@ -17,5 +23,5 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_test_lambda" {
     action = var.action2
     function_name = aws_lambda_function.test_lambda.function_name
     principal = var.principle2
-    source_arn = "${aws_cloudwatch_event_rule.lambda_trigger_every_five_minutes.arn}"
+    source_arn = "${aws_cloudwatch_event_rule.lambda_trigger_after_crawler_succeded.arn}"
 }
