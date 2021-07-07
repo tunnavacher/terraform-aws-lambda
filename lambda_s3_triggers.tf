3a64f43ad8884cd0e6152c9c7c135e5cdf8276c8
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-07-07 18:51:01 +0530

    Update lambda_s3_triggers.tf

--- lambda_s3_triggers.tf
+++ lambda_s3_triggers.tf
@@ -3,7 +3,7 @@
 resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
 bucket = var.event_bucket_name
 lambda_function {
-lambda_function_arn = aws_lambda_function.test_lambda.arn
+lambda_function_arn = aws_lambda_function.crawlerstart_lambda.arn
 events              = [var.event_type]
 filter_prefix       = var.prefix
 filter_suffix       = var.suffix
@@ -13,7 +13,7 @@ filter_suffix       = var.suffix
 resource "aws_lambda_permission" "test" {
 statement_id  = var.statement_id1
 action        = var.action1
-function_name = aws_lambda_function.test_lambda.function_name
+function_name = aws_lambda_function.crawlerstart_lambda.function_name
 principal = var.principle1
 source_arn = var.source_arn
 }
